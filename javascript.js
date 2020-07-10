b8ce38316b658a28e2d964f5d5765bf2cacc52c7
Vijayakumar Sundar <42805691+vijaysundar2701@users.noreply.github.com>
2020-07-10 11:39:59 +0530

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,60 @@
+var i=0;
+function personal()
+{
+	 document.getElementById("per").style.display = "block";
+	 document.getElementById("edu").style.display = "none";
+	 document.getElementById("tech").style.display = "none";
+	 document.getElementById("aca").style.display = "none";
+	 document.getElementById("pro").style.display = "none";
+	 document.getElementById("msg").style.display = "none";
+}
+function educational()
+{
+	 document.getElementById("per").style.display = "none";
+	 document.getElementById("edu").style.display = "block";
+	 document.getElementById("tech").style.display = "none";
+	 document.getElementById("aca").style.display = "none";
+	 document.getElementById("pro").style.display = "none";
+	 document.getElementById("msg").style.display = "none";
+	 
+}
+function technical()
+{
+	 document.getElementById("per").style.display = "none";
+	 document.getElementById("edu").style.display = "none";
+	 document.getElementById("tech").style.display = "block";
+	 document.getElementById("aca").style.display = "none";
+	 document.getElementById("pro").style.display = "none";
+	 document.getElementById("msg").style.display = "none";
+	 
+}
+function academic()
+{
+     document.getElementById("per").style.display = "none";
+	 document.getElementById("edu").style.display = "none";
+	 document.getElementById("tech").style.display = "none";
+	 document.getElementById("aca").style.display = "block";
+	 document.getElementById("pro").style.display = "none";
+	 document.getElementById("msg").style.display = "none";
+	 
+}
+function project()
+{
+	 document.getElementById("per").style.display = "none";
+	 document.getElementById("edu").style.display = "none";
+	 document.getElementById("tech").style.display = "none";
+	 document.getElementById("aca").style.display = "none";
+	 document.getElementById("pro").style.display = "block";
+	 document.getElementById("msg").style.display = "none";
+	 
+}
+function contact()
+{
+	 document.getElementById("per").style.display = "none";
+	 document.getElementById("edu").style.display = "none";
+	 document.getElementById("tech").style.display = "none";
+	 document.getElementById("aca").style.display = "none";
+	 document.getElementById("pro").style.display = "none";
+	 document.getElementById("msg").style.display = "block";
+	 
+}
